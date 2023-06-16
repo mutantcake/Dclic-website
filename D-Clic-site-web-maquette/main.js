468784dad221e95e6fd40e47b3c92a81bda253a5
Damson <damsonweb@gmail.com>
2023-06-16 15:25:06 +0000

    Just another commit

--- D-Clic-site-web-maquette/main.js
+++
@@ -1,6 +0,0 @@
-const menuHamburger = document.querySelector(".menu-btn");
-        const navLinks = document.querySelector(".nav-links");
- 
-        menuHamburger.addEventListener('click',()=>{
-        navLinks.classList.toggle('mobile-menu')
-        });
